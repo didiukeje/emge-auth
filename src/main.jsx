f92dfa481959db00b02598d1a4e276dfa6e5094a
Donnuel97 <emmanuelemesue97@gmail.com>
2025-02-15 23:18:14 +0100

    Main mod

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
@@ -17,8 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Router>
         <ToastContainer />
         <Routes>
-          {/* Redirect from / to /signup */}
-          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/" element={<App />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route
